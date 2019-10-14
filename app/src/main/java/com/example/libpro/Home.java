e9354801212c8dfe7588248fe86c3581aa09dd32
azooz <azooz1170@gmail.com>
2019-10-15 00:05:20 +0300

    home-upload-setting

--- app/src/main/java/com/example/libpro/Home.java
+++ app/src/main/java/com/example/libpro/Home.java
@@ -3,12 +3,36 @@ package com.example.libpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
-
+    ListView listViewHome ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setTitle("Home");
+
+
+        listViewHome = (ListView) findViewById(R.id.listviewhome);
+
+        final ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
+        arrayList.add("6");
+        arrayList.add("7");
+        arrayList.add("8");
+        arrayList.add("9");
+        arrayList.add("10");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listViewHome.setAdapter(arrayAdapter);
     }
 }
