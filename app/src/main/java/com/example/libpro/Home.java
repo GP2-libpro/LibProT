7dc2caf1f66c62ecd6c7dc9c5a83c862825f604a
azooz <azooz1170@gmail.com>
2019-10-17 04:15:09 +0300

    created activities to display books& rate books created search function

--- app/src/main/java/com/example/libpro/Home.java
+++ app/src/main/java/com/example/libpro/Home.java
@@ -4,23 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Home extends AppCompatActivity {
-   private Button rating;
+    private  ArrayAdapter arrayAdapter;
+    private Button rating;
+   EditText search;
     ListView listViewHome ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setTitle("Home");
+
+        search= (EditText) findViewById(R.id.search);
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                (Home.this).arrayAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
         listViewHome = (ListView) findViewById(R.id.listviewhome);
 
@@ -37,10 +64,10 @@ public class Home extends AppCompatActivity {
         arrayList.add("9");
         arrayList.add("10");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+         arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
         listViewHome.setAdapter(arrayAdapter);
 
-        /*****************/
+
 
 
 
@@ -59,6 +86,7 @@ public class Home extends AppCompatActivity {
 
             }
         });
+
 
     }
 }
